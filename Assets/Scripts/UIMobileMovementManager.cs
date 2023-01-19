e076c0ab675bf59e47ed6bcddbf36ac6ce461f83
Torcu <45042365+Exater5@users.noreply.github.com>
2023-01-19 17:45:20 +0100

    Fixing Mobile Controls

--- Assets/Scripts/UIMobileMovementManager.cs
+++ Assets/Scripts/UIMobileMovementManager.cs
@@ -6,40 +6,22 @@ public class UIMobileMovementManager : MonoBehaviour
 {
     private bool _pressing;
     GameFunctionsController.Direction _currentDirection;
-    public void MoveUp()
+    public void Move(int targetDirIndex)
     {
         _pressing = true;
-        _currentDirection = GameFunctionsController.Direction.Up;
-    }
-    public void MoveDown()
-    {
-        _pressing = true;
-        _currentDirection = GameFunctionsController.Direction.Down;
-    }
-    public void MoveLeft()
-    {
-        _pressing = true;
-        _currentDirection = GameFunctionsController.Direction.Left;
-    }
-    public void MoveRight()
-    {
-        _pressing = true;
-        _currentDirection = GameFunctionsController.Direction.Right;
+        _currentDirection = (GameFunctionsController.Direction)targetDirIndex;
     }
 
     public void OnButtonExit()
     {
         _pressing = false;
+        GameEvents.OnStopMovement.Invoke();
     }
     private void Update()
     {
         if (_pressing)
         {
             GameEvents.MobileMovement.Invoke(_currentDirection);
-        }
-        else 
-        {
-            GameEvents.OnStopMovement.Invoke();
         }
     }
 
