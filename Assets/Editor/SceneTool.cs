73f9644c0a1389c04aec1f7a43d941469a5edc6a
Torcu <45042365+Exater5@users.noreply.github.com>
2023-01-26 18:02:22 +0100

    Ordering Scenes

--- Assets/Editor/SceneTool.cs
+++ Assets/Editor/SceneTool.cs
@@ -6,38 +6,38 @@ public class SceneTool : MonoBehaviour{
 public static void LoadScene0()
 {
  LoadScene("Assets/Scenes/Contrast_Room.unity");
-}[MenuItem("Open Scene/Level0")]
+}[MenuItem("Open Scene/Map Creator")]
 public static void LoadScene1()
 {
- LoadScene("Assets/Scenes/Level0.unity");
-}[MenuItem("Open Scene/Level1")]
+ LoadScene("Assets/Scenes/Map Creator.unity");
+}[MenuItem("Open Scene/Start")]
 public static void LoadScene2()
 {
- LoadScene("Assets/Scenes/Level1.unity");
-}[MenuItem("Open Scene/Level2")]
+ LoadScene("Assets/Scenes/Start.unity");
+}[MenuItem("Open Scene//Levels/Level0")]
 public static void LoadScene3()
 {
- LoadScene("Assets/Scenes/Level2.unity");
-}[MenuItem("Open Scene/Level3")]
+ LoadScene("Assets/Scenes/Levels/Level0.unity");
+}[MenuItem("Open Scene//Levels/Level1")]
 public static void LoadScene4()
 {
- LoadScene("Assets/Scenes/Level3.unity");
-}[MenuItem("Open Scene/Map Creator")]
+ LoadScene("Assets/Scenes/Levels/Level1.unity");
+}[MenuItem("Open Scene//Levels/Level2")]
 public static void LoadScene5()
 {
- LoadScene("Assets/Scenes/Map Creator.unity");
-}[MenuItem("Open Scene/Sandbox_Amin")]
+ LoadScene("Assets/Scenes/Levels/Level2.unity");
+}[MenuItem("Open Scene//Levels/Level3")]
 public static void LoadScene6()
 {
- LoadScene("Assets/Scenes/Sandbox_Amin.unity");
-}[MenuItem("Open Scene/Sandbox_Torcu")]
+ LoadScene("Assets/Scenes/Levels/Level3.unity");
+}[MenuItem("Open Scene//Sandbox/Sandbox_Amin")]
 public static void LoadScene7()
 {
- LoadScene("Assets/Scenes/Sandbox_Torcu.unity");
-}[MenuItem("Open Scene/Start")]
+ LoadScene("Assets/Scenes/Sandbox/Sandbox_Amin.unity");
+}[MenuItem("Open Scene//Sandbox/Sandbox_Torcu")]
 public static void LoadScene8()
 {
- LoadScene("Assets/Scenes/Start.unity");
+ LoadScene("Assets/Scenes/Sandbox/Sandbox_Torcu.unity");
 }
 public static void LoadScene(string scenePath)
     {
